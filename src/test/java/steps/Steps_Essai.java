ce136d5cbba119ec87f99cdbe95644cfaadaedf4
ylebreton <yoann.le-breton@gfi.fr>
2019-03-14 10:42:52 +0100

    Ajout des classes premières classes steps et runners pour example

---
+++ src/test/java/steps/Steps_Essai.java
@@ -0,0 +1,11 @@
+package steps;
+
+// Import de la syntaxe Anglaise
+import cucumber.api.java.en.*;
+// Import de la syntaxe Française
+import cucumber.api.java.fr.*;
+
+
+public class Steps_Essai {
+
+}
